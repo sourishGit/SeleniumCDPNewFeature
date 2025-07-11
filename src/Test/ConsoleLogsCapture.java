c58e13aed4f257f760ef3727fd1123ceb91a1dd4
sourish <sourish.vlogger@gmail.com>
2025-07-11 15:09:07 +0530

    added consoleLogsCapture

---
+++ src/Test/ConsoleLogsCapture.java
@@ -0,0 +1,37 @@
+package Test;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.logging.LogEntries;
+import org.openqa.selenium.logging.LogEntry;
+import org.openqa.selenium.logging.LogType;
+
+public class ConsoleLogsCapture {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "C:/Users/SOURISH/eclipse-workspace/selenium-java/files/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+		driver.findElement(By.linkText("Browse Products")).click();
+		driver.findElement(By.linkText("Selenium")).click();
+		driver.findElement(By.cssSelector(".add-to-cart")).click();
+		driver.findElement(By.linkText("Cart")).click();
+		driver.findElement(By.id("exampleInputEmail1")).click();
+		driver.findElement(By.id("exampleInputEmail1")).sendKeys("2");
+		
+		LogEntries entry = driver.manage().logs().get(LogType.BROWSER);
+		List<LogEntry> logs  = entry.getAll();
+		
+		for(LogEntry e: logs)
+		{
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
